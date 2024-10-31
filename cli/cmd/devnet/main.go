7587a0b88a75d84ffe99c0646b6d331c021fe6f0
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-10-31 15:28:16 -0300

    feat: simplify CLI args (#46)
    This PR changes the names `run` and `clean` to `start` and `stop`. It
    also makes the "--config" flag an optional positional argument. It also
    uses "devnet.yaml" as the default config file name so users don't need
    to specify it by default.

--- cli/cmd/devnet/main.go
+++ cli/cmd/devnet/main.go
@@ -1,81 +1,89 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 
 	"github.com/urfave/cli/v2"
-	"gopkg.in/yaml.v3"
 )
 
 var version = "development"
 
-var configFlag = cli.StringFlag{
-	Name:    "config",
-	Aliases: []string{"c"},
-	Usage:   "Load devnet configuration from `FILE`",
-}
-
 func main() {
 	app := cli.NewApp()
 	app.Name = "devnet"
-	app.Usage = "start an AVS development network"
+	app.Usage = "start an AVS devnet"
 	app.Version = version
 
 	app.Commands = append(app.Commands, &cli.Command{
-		Name:   "run",
-		Flags:  []cli.Flag{&configFlag},
-		Action: run,
+		Name:      "start",
+		Usage:     "Start the devnet",
+		Args:      true,
+		ArgsUsage: "[<config-file>]",
+		Flags:     []cli.Flag{},
+		Action:    start,
 	})
 
 	app.Commands = append(app.Commands, &cli.Command{
-		Name:   "clean",
-		Flags:  []cli.Flag{&configFlag},
-		Action: clean,
+		Name:      "stop",
+		Usage:     "Stop the devnet",
+		Args:      true,
+		ArgsUsage: "[<config-file>]",
+		Flags:     []cli.Flag{},
+		Action:    stop,
 	})
 
 	app.Run(os.Args)
 }
 
-func run(ctx *cli.Context) error {
-	argsFile := ctx.String(configFlag.Name)
-	devnetName, err := getDevnetName(argsFile)
+func start(ctx *cli.Context) error {
+	argsFile, devnetName, err := parseArgs(ctx)
 	if err != nil {
 		return err
 	}
 
-	cmd := exec.Command("kurtosis", "run", "../kurtosis_package/", "--enclave", devnetName, "--args-file", argsFile)
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	return cmd.Run()
+	return kurtosisRun("run", "../kurtosis_package/", "--enclave", devnetName, "--args-file", argsFile)
 }
 
-func clean(ctx *cli.Context) error {
-	argsFile := ctx.String(configFlag.Name)
-	devnetName, err := getDevnetName(argsFile)
+func stop(ctx *cli.Context) error {
+	_, devnetName, err := parseArgs(ctx)
 	if err != nil {
 		return err
 	}
-	cmd := exec.Command("kurtosis", "enclave", "rm", "-f", devnetName)
+
+	return kurtosisRun("enclave", "rm", "-f", devnetName)
+}
+
+func parseArgs(ctx *cli.Context) (string, string, error) {
+	args := ctx.Args()
+	if args.Len() > 1 {
+		return "", "", cli.Exit("Expected exactly 1 argument: <config-file>", 1)
+	}
+	argsFile := args.First()
+	var devnetName string
+	if argsFile == "" {
+		argsFile = "devnet.yaml"
+		devnetName = "devnet"
+	} else {
+		devnetName = nameFromArgsFile(argsFile)
+	}
+	if _, err := os.Stat(argsFile); errors.Is(err, os.ErrNotExist) {
+		return "", "", cli.Exit("Config file doesn't exist: "+argsFile, 2)
+	}
+	return argsFile, devnetName, nil
+}
+
+func nameFromArgsFile(argsFile string) string {
+	base := filepath.Base(argsFile)
+	return strings.Split(base, ".")[0]
+}
+
+func kurtosisRun(args ...string) error {
+	cmd := exec.Command("kurtosis", args...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	return cmd.Run()
-}
-
-func getDevnetName(filePath string) (string, error) {
-	if filePath == "" {
-		return "devnet", nil
-	}
-	file, err := os.ReadFile(filePath)
-	if err != nil {
-		return "", err
-	}
-	var devnetConfig struct {
-		Name *string
-	}
-	err = yaml.Unmarshal(file, &devnetConfig)
-	if devnetConfig.Name == nil {
-		return "devnet", err
-	}
-	return *devnetConfig.Name, err
 }
