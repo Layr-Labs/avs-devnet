bee8e52a6caf3073cb5e4a19d12e9194c1499b9f
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-11-06 14:34:45 -0300

    feat: removed usage of Kurtosis CLI

--- src/cmds/stop.go
+++ src/cmds/stop.go
@@ -3,6 +3,7 @@ package cmds
 import (
 	"fmt"
 
+	"github.com/kurtosis-tech/kurtosis/api/golang/engine/lib/kurtosis_context"
 	"github.com/urfave/cli/v2"
 )
 
@@ -12,5 +13,12 @@ func Stop(ctx *cli.Context) error {
 		return cli.Exit(err, 1)
 	}
 	fmt.Println("Stopping devnet...")
-	return kurtosisRun("enclave", "rm", "-f", devnetName)
+	kurtosisCtx, err := kurtosis_context.NewKurtosisContextFromLocalEngine()
+	if err != nil {
+		return cli.Exit(err, 2)
+	}
+	if err = kurtosisCtx.DestroyEnclave(ctx.Context, devnetName); err != nil {
+		return cli.Exit(err, 3)
+	}
+	return err
 }
