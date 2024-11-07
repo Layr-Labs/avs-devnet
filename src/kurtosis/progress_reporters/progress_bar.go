a4f7fd77a6609d3f29a275502984b237ab323bca
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-11-07 18:36:42 -0300

    fix: declare vars and return nil

--- src/kurtosis/progress_reporters/progress_bar.go
+++ src/kurtosis/progress_reporters/progress_bar.go
@@ -32,19 +32,19 @@ func ReportProgress(reporter KurtosisReporter) error {
 			if len(progressInfo.CurrentStepInfo) > 1 {
 				detail = progressInfo.CurrentStepInfo[1]
 			}
-			if err = pb.AddDetail(detail); err != nil {
+			if err := pb.AddDetail(detail); err != nil {
 				return err
 			}
 			if progressInfo.TotalSteps != 0 {
 				if progressInfo.CurrentStepNumber >= progressInfo.TotalSteps {
 					pb.Close()
 					pb = newExecutionProgressBar(-1)
-					if err = pb.RenderBlank(); err != nil {
+					if err := pb.RenderBlank(); err != nil {
 						return err
 					}
 				} else {
 					pb.ChangeMax(int(progressInfo.TotalSteps))
-					if err = pb.Set(int(progressInfo.CurrentStepNumber)); err != nil {
+					if err := pb.Set(int(progressInfo.CurrentStepNumber)); err != nil {
 						return err
 					}
 				}
@@ -77,6 +77,7 @@ func ReportProgress(reporter KurtosisReporter) error {
 			fmt.Fprintln(os.Stderr, line.GetRunFinishedEvent())
 		}
 	}
+	return nil
 }
 
 func newValidationProgressBar() *progressbar.ProgressBar {
