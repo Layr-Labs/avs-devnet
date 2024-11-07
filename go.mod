8ac7c14059d1852e49ab1fed77d5756adc93bf2d
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-11-07 18:22:17 -0300

    feat: add progress bar for `devnet start` cmd

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.4
 
 require (
 	github.com/kurtosis-tech/kurtosis/api/golang v1.4.1
+	github.com/schollz/progressbar/v3 v3.17.0
 	github.com/stretchr/testify v1.9.0
 	github.com/tidwall/gjson v1.18.0
 	github.com/urfave/cli/v2 v2.27.5
@@ -27,9 +28,11 @@ require (
 	github.com/kurtosis-tech/kurtosis/path-compression v0.0.0-20240307154559-64d2929cd265 // indirect
 	github.com/kurtosis-tech/stacktrace v0.0.0-20211028211901-1c67a77b5409 // indirect
 	github.com/mholt/archiver v3.1.1+incompatible // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/nwaples/rardecode v1.1.3 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
@@ -40,6 +43,7 @@ require (
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/term v0.25.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20241007155032-5fefd90f89a9 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241007155032-5fefd90f89a9 // indirect
--- src/cmds/start.go
+++ src/cmds/start.go
@@ -11,8 +11,10 @@ import (
 	"github.com/Layr-Labs/avs-devnet/src/cmds/flags"
 	"github.com/Layr-Labs/avs-devnet/src/config"
 	"github.com/Layr-Labs/avs-devnet/src/kurtosis"
+	"github.com/kurtosis-tech/kurtosis/api/golang/core/kurtosis_core_rpc_api_bindings"
 	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/enclaves"
 	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/starlark_run_config"
+	"github.com/schollz/progressbar/v3"
 	"github.com/urfave/cli/v2"
 )
 
@@ -67,14 +69,87 @@ func startDevnet(ctx *cli.Context, pkgName, devnetName string, configPath string
 	starlarkConfig := starlark_run_config.NewRunStarlarkConfig()
 	starlarkConfig.SerializedParams = string(configBytes)
 
-	var result *enclaves.StarlarkRunResult
+	var responseChan chan *kurtosis_core_rpc_api_bindings.StarlarkRunResponseLine
+	// TODO: use cancel func if needed
+	// var cancel context.CancelFunc
 	// TODO: stream result lines and give user progress updates
 	if strings.HasPrefix(pkgName, "github.com/") {
-		result, err = enclaveCtx.RunStarlarkRemotePackageBlocking(ctx.Context, pkgName, starlarkConfig)
+		responseChan, _, err = enclaveCtx.RunStarlarkRemotePackage(ctx.Context, pkgName, starlarkConfig)
 	} else {
-		result, err = enclaveCtx.RunStarlarkPackageBlocking(ctx.Context, pkgName, starlarkConfig)
+		responseChan, _, err = enclaveCtx.RunStarlarkPackage(ctx.Context, pkgName, starlarkConfig)
 	}
-	_ = result
+	if err != nil {
+		return err
+	}
+
+	// state := "validation"
+	pb := newValidationProgressBar()
+	if err = pb.RenderBlank(); err != nil {
+		return err
+	}
+	for line := range responseChan {
+		if line.GetProgressInfo() != nil {
+			// It's a progress info
+			// fmt.Println(line.GetProgressInfo())
+			progressInfo := line.GetProgressInfo()
+			if len(progressInfo.CurrentStepInfo) == 0 {
+				continue
+			}
+			if progressInfo.CurrentStepInfo[0] == "Starting validation" {
+				continue
+			}
+			fmt.Fprintln(os.Stderr, "progress: ", progressInfo)
+
+			pb.Describe(progressInfo.CurrentStepInfo[0])
+			detail := ""
+			if len(progressInfo.CurrentStepInfo) > 1 {
+				detail = progressInfo.CurrentStepInfo[1]
+			}
+			if err = pb.AddDetail(detail); err != nil {
+				return err
+			}
+			if progressInfo.TotalSteps != 0 {
+				if progressInfo.CurrentStepNumber >= progressInfo.TotalSteps {
+					pb.Close()
+					pb = newExecutionProgressBar(-1)
+					if err = pb.RenderBlank(); err != nil {
+						return err
+					}
+				} else {
+					pb.ChangeMax(int(progressInfo.TotalSteps))
+					if err = pb.Set(int(progressInfo.CurrentStepNumber)); err != nil {
+						return err
+					}
+				}
+			}
+		}
+		if line.GetInstruction() != nil {
+			// It's an instruction
+			fmt.Fprintln(os.Stderr, line.GetInstruction().Description)
+		}
+		if line.GetInfo() != nil {
+			// It's an info
+			fmt.Fprintln(os.Stderr, "INFO:", line.GetInfo().InfoMessage)
+		}
+		if line.GetWarning() != nil {
+			// It's a warning
+			fmt.Fprintln(os.Stderr, line.GetWarning().WarningMessage)
+		}
+		if line.GetInstructionResult() != nil {
+			// It's an instruction result
+			fmt.Fprintln(os.Stderr, line.GetInstructionResult())
+		}
+		if line.GetError() != nil {
+			// It's an error
+			// pb.Exit()
+			fmt.Fprintln(os.Stderr, line.GetError())
+		}
+		if line.GetRunFinishedEvent() != nil {
+			// It's a run finished event
+			// pb.Close()
+			fmt.Fprintln(os.Stderr, line.GetRunFinishedEvent())
+		}
+	}
 
 	fmt.Println("Devnet started!")
 	return err
@@ -135,4 +210,62 @@ func buildDockerImages(config config.DevnetConfig) error {
 		errs = append(errs, <-errChan)
 	}
 	return errors.Join(errs...)
+}
+
+func newValidationProgressBar() *progressbar.ProgressBar {
+	pb := progressbar.NewOptions(
+		-1,
+		progressbar.OptionSetMaxDetailRow(1),
+		progressbar.OptionShowDescriptionAtLineEnd(),
+		progressbar.OptionClearOnFinish(),
+		progressbar.OptionSetElapsedTime(true),
+		progressbar.OptionSetPredictTime(false),
+		// TODO: use Stderr for progress bar
+		// progressbar.OptionSetWriter(os.Stderr),
+		progressbar.OptionSetWriter(os.Stdout),
+		progressbar.OptionShowCount(),
+		// progressbar.OptionOnCompletion(func() {
+		// 	fmt.Fprint(os.Stderr, "\n")
+		// }),
+		progressbar.OptionSpinnerType(14),
+		progressbar.OptionSetWidth(20),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[green]=[reset]",
+			SaucerHead:    "[green]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+		progressbar.OptionEnableColorCodes(true),
+	)
+	return pb
+}
+
+func newExecutionProgressBar(steps int) *progressbar.ProgressBar {
+	pb := progressbar.NewOptions(
+		steps,
+		progressbar.OptionSetMaxDetailRow(2),
+		progressbar.OptionShowDescriptionAtLineEnd(),
+		progressbar.OptionClearOnFinish(),
+		progressbar.OptionSetElapsedTime(true),
+		progressbar.OptionSetPredictTime(false),
+		// TODO: use Stderr for progress bar
+		// progressbar.OptionSetWriter(os.Stderr),
+		progressbar.OptionSetWriter(os.Stdout),
+		progressbar.OptionShowCount(),
+		// progressbar.OptionOnCompletion(func() {
+		// 	fmt.Fprint(os.Stderr, "\n")
+		// }),
+		progressbar.OptionSpinnerType(14),
+		progressbar.OptionSetWidth(50),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "[green]=[reset]",
+			SaucerHead:    "[green]>[reset]",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+		progressbar.OptionEnableColorCodes(true),
+	)
+	return pb
 }
