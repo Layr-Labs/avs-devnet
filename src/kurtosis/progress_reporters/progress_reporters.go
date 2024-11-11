1e37310a1ed568f88883ff06878c7acfc0602320
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-11-11 16:58:19 -0300

    fix: transition Interpretation -> Validation wasn't triggering

--- src/kurtosis/progress_reporters/progress_reporters.go
+++ src/kurtosis/progress_reporters/progress_reporters.go
@@ -86,7 +86,7 @@ func ReportProgress(reporter Reporter, responseChan chan KurtosisResponse) error
 					return err
 				}
 				continue
-			} else if strings.HasPrefix(description, "Validating plan") && progressInfo.TotalSteps == 0 && progressInfo.TotalSteps != 0 {
+			} else if strings.HasPrefix(description, "Validating plan") && state == Interpretation && progressInfo.TotalSteps != 0 {
 				state = Validation
 				totalSteps = progressInfo.TotalSteps
 				if err := reporter.ReportValidationStart(int(totalSteps)); err != nil {
